d0c237eb7fb0f051719595ee1f21598d4569c8b4
tomiyasu0428 <aolarena16@gmail.com>
2024-12-03 13:32:18 +0900

    fix_B

--- b-6.py
+++ b-6.py
@@ -1,13 +1,13 @@
 import random
 
-saikoro_N = int(input("サイコロの面の数は?: "))
-saikoro_M = int(input("何回ふりますか？: "))
+saikoro_n = int(input("サイコロの面の数は?: "))
+saikoro_m = int(input("何回ふりますか？: "))
 
 result = []
 
 play_time = 1
-while play_time <= saikoro_M:
-    saikoro_number = random.randint(1, saikoro_N)
+while play_time <= saikoro_m:
+    saikoro_number = random.randint(1, saikoro_n)
     result.append(saikoro_number)
     play_time += 1
 
