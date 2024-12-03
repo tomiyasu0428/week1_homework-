13049fec4620a7908ed28235b70166af80f2e565
tomiyasu0428 <aolarena16@gmail.com>
2024-12-03 16:01:16 +0900

    fix_B3

--- b-6.py
+++ b-6.py
@@ -5,7 +5,7 @@ saikoro_m = int(input("何回ふりますか？: "))
 
 result = []
 
-play_time = 0
+
 for play_time in range(0, saikoro_m):
     saikoro_number = random.randint(1, saikoro_n)
     result.append(saikoro_number)
