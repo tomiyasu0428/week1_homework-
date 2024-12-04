baad0ada75a20236426ad4277937380bef8dcf08
tomiyasu0428 <aolarena16@gmail.com>
2024-12-04 10:15:21 +0900

    fix_D

--- d-1.py
+++ d-1.py
@@ -1,20 +1,23 @@
+import math
+
+
 class Circle:
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        return 3.14 * self.radius**2
+        return math.pi * self.radius**2
 
     def perimeter(self):
-        return 2 * 3.14 * self.radius
+        return 2 * math.pi * self.radius
 
 
 # 半径1の円
 circle1 = Circle(radius=1)
-print(circle1.area())  # 3.14
-print(circle1.perimeter())  # 6.28
+print(round(circle1.area(), 2))  # 3.14
+print(round(circle1.perimeter(), 2))  # 6.28
 
 # 半径3の円
 circle3 = Circle(radius=3)
-print(circle3.area())  # 28.27
-print(circle3.perimeter())  # 18.85
+print(round(circle3.area(), 2))  # 28.27
+print(round(circle3.perimeter(), 2))  # 18.85
