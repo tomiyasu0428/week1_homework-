9199ccdf23f761ebd30b07caa2898ab168e8e621
tomiyasu0428 <aolarena16@gmail.com>
2024-12-04 10:00:56 +0900

    fin_C

--- customer.py
+++ customer.py
@@ -8,20 +8,25 @@ class Customer:
         return f'"{self.first_name} {self.family_name}"'
 
     def entry_fee(self):
-        if self.age <= 20:
+        if 3 > self.age <= 20:
             return 1000
         elif 20 <= self.age < 65:
             return 1500
-        else:
+        elif self.age <= 3:
+            return 0
+        elif 65 <= self.age < 75:
             return 1200
+        else:
+            return 500
 
     def info_csv(self):
-        return f'"{self.full_name()},{self.age},{self.entry_fee()}"'
+        return f"{self.full_name()}|{self.age}|{self.entry_fee()}"
 
 
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
 tom = Customer(first_name="Tom", family_name="Ford", age=57)
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=75)
+michelle = Customer(first_name="Michelle", family_name="Tanner", age=3)
 
 print(ken.full_name())
 print(tom.full_name())
@@ -33,6 +38,7 @@ print(ieyasu.age)
 print(ken.entry_fee())
 print(tom.entry_fee())
 print(ieyasu.entry_fee())
+print(michelle.entry_fee())
 
 print(ken.info_csv())
 print(ken.info_csv())
